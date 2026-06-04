929707fab38cea2b6fd7b06b36ab6e36bd0dded2
Aditya Srivastava <mk4572786@gmail.com>
2026-06-04 10:23:42 +0530

    first basics of C

---
+++ Chapter1.c
@@ -0,0 +1,14 @@
+// Basics in C,contains a sum of 2 numbers program using basics//
+#include <stdio.h>
+
+int main() {
+   int a,b;
+   printf("Enter a");
+   scanf("%d",&a);
+
+   printf("Enter b");
+   scanf("%d",&b);
+    
+   printf("Sum is %d",a + b);
+    return 0;
+}

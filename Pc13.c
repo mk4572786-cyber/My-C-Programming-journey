658f32f747ddf2230074f1c92465d46572423c58
Aditya Srivastava <mk4572786@gmail.com>
2026-08-26 11:18:21 +0530

    Questions done based on C programming

---
+++ Pc13.c
@@ -0,0 +1,11 @@
+// Calculate the sum of all numbers between 5 to 50//
+
+#include<stdio.h>
+int main () {
+    int sum =0;
+    for( int i=5; i<=50 ; i++) {
+        sum = sum +i;
+    }
+    printf("sum is %d", sum);
+    return 0;
+}
---
+++ Pc14.c
@@ -0,0 +1,20 @@
+// Basic function program to print 'Hello' and 'Goodbye'
+
+#include<stdio.h>
+
+void printHello();
+void printGoodbye();
+
+int main () {
+    printHello();
+    printGoodbye();
+    return 0;
+}
+
+void printHello () {
+    printf("Hello\n");
+}
+
+void printGoodbye () {
+    printf("GoodBye\n");
+}

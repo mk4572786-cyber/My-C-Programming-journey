7908383fee276447be94b15c2227883f8b8bbae1
Aditya Srivastava <mk4572786@gmail.com>
2026-09-13 18:16:22 +0530

    Basic problems for functions

---
+++ Pc16.c
@@ -0,0 +1,11 @@
+// Use library functions to calculate the square of a no.
+//given by the user.
+
+#include<stdio.h>
+#include<math.h>
+
+int main () {
+     int n =4 ;
+     printf("%f", pow(n,2));
+     return 0;
+}
